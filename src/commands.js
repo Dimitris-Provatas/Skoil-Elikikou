ab23d4680e9b92a8e101c62a0001d9bf3bb2dcf7
Dimitris-Provatas <james.provatas@gmail.com>
2021-03-11 17:03:09 +0200

    files and work on the functions

--- src/commands.js
+++ src/commands.js
@@ -11,27 +11,29 @@ function helpMessage(author)
     commands.forEach(command =>
     {
         help += `-> ${command}\r\n`;
-
-        if (command === "play")
-        {
-            memeDirectories.forEach((memeDirectory, idx) =>
-            {
-                help += ` |-> ${memeDirectory}\r\n`;
-                const memeDirectoryContents = fs.readdirSync(memeDirectoryPath + memeDirectory + "/");
-                if (idx === memeDirectories.length - 1)
-                    memeDirectoryContents.forEach(memeDirectoryContent => {
-                        help += `     |-> ${memeDirectoryContent}\r\n`;
-                    });
-                else
-                    memeDirectoryContents.forEach(memeDirectoryContent => {
-                        help += ` |   |-> ${memeDirectoryContent}\r\n`;
-                    });
-            });
-        }
     });
     help += "\`\`\`";
 
     return help;
+}
+
+function filesMessage(author)
+{
+    var available = `${author} οι διαθέσιμες εντολές είναι:\r\n\`\`\`\r\n`;
+
+    memeDirectories.forEach((memeDirectory, idx) =>
+    {
+        available += `-> ${memeDirectory}\r\n`;
+        const memeDirectoryContents = fs.readdirSync(memeDirectoryPath + memeDirectory + "/");
+        memeDirectoryContents.forEach(memeDirectoryContent => {
+            available += `${memeDirectoryContent}, `;
+        });
+        available += "\r\n\r\n";
+    });
+
+    available += "\`\`\`";
+
+    return available;
 }
 
 function getMemeDirectories(path)
@@ -43,20 +45,21 @@ function getMemeDirectories(path)
 
 const commands = [
     "help",
-    "play"
+    "files_available",
+    "play",
 ]
 
 async function PerformCommands(bot, message)
 {
     var command = message.content.substr(message.content.indexOf(' ') + 1);
 
-    if (command === "play")
+    if (command.startsWith("play "))
     {
         command = command.split(" ");
         const dir = command[1];
         const file = command[2];
 
-        eval("play(bot, message, func, dir, file);");
+        eval("play(bot, message, dir, file);");
     }
     else
     {
@@ -67,7 +70,7 @@ async function PerformCommands(bot, message)
 
 async function play(bot, message, dir, file)
 {
-    const vc = message.member.voice.channel;
+    const vc = message.member.voice.channel || null;
     if (!vc)
     {
         await message.channel.send(`${message.author} δεν είσαι σε Voice Channel.`);
@@ -79,18 +82,19 @@ async function play(bot, message, dir, file)
 
     if (!isPlaying)
     {
-        vc.join().then(connection =>
+        vc.join().then(async connection =>
         {
             isPlaying = true;
 
             while (playQueue.length > 0)
             {
                 const target = playQueue.shift();
-                await message.channel.send(`Now Playing: ${target.substr(playQueue[0].lastIndexOf('/'))}`);
+                await message.channel.send(`Now Playing: ${target.substr(target.lastIndexOf('/')).substr(1)}`);
                 await connection.play(target);
             }
 
-            await vc.leave().then(() => { isPlaying = false; });
+            await vc.leave();
+            isPlaying = false;
         }).catch(err => { console.log(err); });
     }
 }
@@ -98,6 +102,11 @@ async function play(bot, message, dir, file)
 async function help(bot, message)
 {
     await message.channel.send(helpMessage(message.author));
+}
+
+async function files_available(bot, message)
+{
+    await message.channel.send(filesMessage(message.author));
 }
 
 module.exports = {
