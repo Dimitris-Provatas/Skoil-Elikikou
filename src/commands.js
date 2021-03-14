4080ac56f27f5b23295234c8924bdb878ba6e618
Dimitris Provatas <53426786+Dimitris-Provatas@users.noreply.github.com>
2021-03-14 11:21:36 +0000

    minor oversight fixed

--- src/commands.js
+++ src/commands.js
@@ -61,8 +61,7 @@ async function PerformCommands(bot, message)
     var command = message.content.substr(message.content.indexOf(' ') + 1);
     if (
         command.indexOf('{') !== -1 &&
-        command.indexOf('}') !== -1 &&
-        command.indexOf(';') !== -1
+        command.indexOf('}') !== -1
     )
     {
         await message.channel.send(`${message.author} ξέρω τι πας να κάνεις εκεί, κακό! Kρίμα που τώρα οι <@&488730147894198273> θα μάθουν τι πήγες να κάνεις!`);
