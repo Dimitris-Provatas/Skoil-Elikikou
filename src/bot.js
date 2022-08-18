a7f78ae8bd73f4f01c5f1991ae2aa286e88f1517
Dimitris Provatas <53426786+Dimitris-Provatas@users.noreply.github.com>
2022-08-18 18:31:18 +0000

    migrating to .env for the token to be safe

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,6 @@
-// const fs = require("fs")
-const { token } = require("./secrets.json");
+const path = require("path");
+const dotenv = require('dotenv');
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 const Discord = require("discord.js");
 const bot = new Discord.Client();
@@ -8,39 +9,39 @@ const handler = require("./handler");
 const helper = require("./helper");
 
 function botInit() {
-  bot.login(token).then(
-    () => {},
-    (error) => {
-      console.log(error);
-    },
-  );
+    bot.login(process.env.TOKEN).then(
+        () => { console.log("Bot is online!") },
+        (error) => {
+            console.log(error);
+        },
+    );
 
-  bot.on("ready", async () => {
-    const time = helper.GetTime();
-    console.info(`${time}: Logged in as ${bot.user.tag}!`);
-    bot.user.setActivity("memes flying out!", {
-      type: "WATCHING",
+    bot.on("ready", async () => {
+        const time = helper.GetTime();
+        console.info(`${time}: Logged in as ${bot.user.tag}!`);
+        bot.user.setActivity("memes flying out!", {
+            type: "WATCHING",
+        });
     });
-  });
 
-  bot.on("guildCreate", (guild) => {
-    guild.systemChannel.send("ΜΙΜΣ ΓΙΑ ΟΛΟΥΣ ΤΩΡΑΑΑΑΑΑ!!!!!!");
-  });
+    bot.on("guildCreate", (guild) => {
+        guild.systemChannel.send("ΜΙΜΣ ΓΙΑ ΟΛΟΥΣ ΤΩΡΑΑΑΑΑΑ!!!!!!");
+    });
 
-  bot.on("message", async (message) => {
-    // console.log(message.content);
+    bot.on("message", async (message) => {
+        // console.log(message.content);
 
-    // handle bots
-    if (message.author.bot) return;
-    // handle DMs
-    else if (message.channel.type == "dm") await handler.HandleDM(bot, message);
-    else if (!message.content.toLowerCase().startsWith("skoil")) return;
-    // handle not bot requests channel
-    else if (!message.channel.name.includes("bot-"))
-      await handler.NotRightChannel(bot, message);
-    // handle humans
-    else await handler.HandleHuman(bot, message);
-  });
+        // handle bots
+        if (message.author.bot) return;
+        // handle DMs
+        else if (message.channel.type == "dm") await handler.HandleDM(bot, message);
+        else if (!message.content.toLowerCase().startsWith("skoil")) return;
+        // handle not bot requests channel
+        else if (!message.channel.name.includes("bot-"))
+            await handler.NotRightChannel(bot, message);
+        // handle humans
+        else await handler.HandleHuman(bot, message);
+    });
 }
 
 module.exports = botInit;
